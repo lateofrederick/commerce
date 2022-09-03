6d5f62e6dd138b3bf771f7e64fa51fcbcd1ec08d
lateofrederick <lateofrederick@gmail.com>
2022-09-03 06:41:34 +0000

    add url to allowed hosts

--- store/settings.py
+++ store/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['lateofrederick.pythonanywhere.com']
 
 
 # Application definition
