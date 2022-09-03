b4cb1515c2688949ca26cfb2bc146c23c5d5f631
lateofrederick <lateofrederick@gmail.com>
2022-09-03 06:50:11 +0000

    remove settings

--- store/wsgi.py
+++ store/wsgi.py
@@ -8,17 +8,8 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-import sys
-
-path = '/home/lateofrederick/commerce'
-if path not in sys.path:
-    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-
-project_folder = os.path.expanduser('~/commerce')
-load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store.settings')
 
